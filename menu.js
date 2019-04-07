0cccc47f6969f2bcdfc3f336eec23f8db35a7d16
qzfing <43680201+qzfing@users.noreply.github.com>
2019-04-06 22:40:41 -0400

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,37 @@
+$(document).ready(main);
+
+var contador = 1  ;
+
+function main(){
+
+
+
+	$('.menubar').click(function(){
+
+
+		if (contador == 1){
+			$('nav').animate({
+				left:'0'
+			});
+
+
+
+         contador = 0;
+
+		} else {
+
+			contador = 1;
+
+			$('nav').animate({
+				left:'-100%'
+			});
+
+
+		}
+
+
+
+	});
+};
+
+
---
+++ submenu.js
@@ -0,0 +1,20 @@
+$(document).ready(main);
+
+function main(){
+
+	$('.sub1').click(function(){
+
+
+		if (1 == 1){
+
+			$('nav').animate({
+				left:'-100%'
+			});
+
+		} 
+
+
+	});
+};
+
+
